cce0d6910aa3779ef100cb7f7b72c258ac5c3313
Mayank Joshi <mayank2000j@gmail.com>
2022-06-01 10:56:01 +0530

    completed addion functionality

---
+++ admin/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeQFkvmZiH5DC9aWlDNBw6y9JmzFfz4N4",
+  authDomain: "digicart-6f8ab.firebaseapp.com",
+  projectId: "digicart-6f8ab",
+  storageBucket: "digicart-6f8ab.appspot.com",
+  messagingSenderId: "877771459515",
+  appId: "1:877771459515:web:1eac4ba8aec64eade8129e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
